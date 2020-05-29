a8677c207fb0e8e6f983d03633ff382126d98a92
Sophie Mission <smnectar@gmail.com>
2020-05-29 07:19:41 +0200

    blog first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.header').height($(window).height());
+})
+
+/*Comment form*/
+
+$(document).ready(function(){
+
+
+    $("[data-toggle=tooltip]").tooltip();
+});
